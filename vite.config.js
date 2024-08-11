6b64e621519929e81e1ca966b3a11f5c2f3d6782
Lauro.sa <Lauro.sa@outlook.com>
2024-08-11 23:50:11 +0000

    Navbar para mobil

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/web-sal/', // Agrega esta línea con el nombre de tu repositorio
+  base: '/web-sal/', // Asegúrate de que el nombre sea exactamente igual al del repositorio
   plugins: [react()],
 })
