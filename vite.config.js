c2e1cb990b7d031842a4bf57911d18160e18483f
Lauro.sa <Lauro.sa@outlook.com>
2024-08-13 00:11:44 +0000

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // Asegúrate de que el nombre sea exactamente igual al del repositorio
+  base: '/web-sal/', // Reemplaza 'web-sal' con el nombre de tu repositorio
   plugins: [react()],
 })
