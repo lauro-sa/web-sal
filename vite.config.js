d2153125b29c7f198da1ce5b7dd73974d4311b3b
Lauro.sa <Lauro.sa@outlook.com>
2024-11-04 21:39:53 +0000

    Para cargar web en hostiguer 01

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/web-sal/', 
-  assetsInclude: ['**/*.pdf'],
+  assetsInclude: ['**/*.pdf'], // Para incluir archivos PDF en los assets
 })
