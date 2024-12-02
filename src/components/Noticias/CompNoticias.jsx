b7da55648765e531bc136cb355d3d18b87c1b953
lauro-sa <108936697+lauro-sa@users.noreply.github.com>
2024-12-01 23:52:38 -0300

    API de noticias funcionando, corrección de títulos y márgenes por el NavbarUsuario, protección de rutas actualizado

--- src/components/Noticias/CompNoticias.jsx
+++ src/components/Noticias/CompNoticias.jsx
@@ -5,12 +5,17 @@ import React from "react";
 
 function CompNoticias({ title, description, url, imageUrl }) {
   // Condición para verificar la presencia y no vacuidad de los campos 'title' y 'description'
-  if (!title || !description || title.trim() === "" || description.trim() === "") {
+  if (
+    !title ||
+    !description ||
+    title.trim() === "" ||
+    description.trim() === ""
+  ) {
     return null; // No renderiza nada si no cumplen los criterios
   }
 
   return (
-    <div className="shadow-md rounded p-6 mb-4 flex flex-col items-center text-white bg-violeta-marca bg-opacity-30 rounded-lg">
+    <div className="shadow-md p-6 mb-4 flex flex-col items-center text-white bg-violeta-marca bg-opacity-30 rounded-lg">
       {/* Renderiza la imagen si existe una URL válida */}
       {imageUrl && (
         <img
@@ -20,8 +25,10 @@ function CompNoticias({ title, description, url, imageUrl }) {
         />
       )}
       <div className="text-center">
-        <h2 className="text-xl font-bold mb-2">{title}</h2> // Título de la noticia
-        <p className="mb-4 text-l">{description}</p> // Descripción breve de la noticia
+        <h2 className="text-xl font-bold mb-2">{title}</h2> // Título de la
+        noticia
+        <p className="mb-4 text-l">{description}</p> // Descripción breve de la
+        noticia
         <a
           href={url}
           target="_blank"
